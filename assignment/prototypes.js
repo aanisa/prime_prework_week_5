eefc5c2f5f3e175860ad4f7e554691062568642d
Anisa <Anisa@Anisas-MacBook-Pro.local>
2017-02-21 15:43:20 -0600

    Assingment Completed

--- assignment/prototypes.js
+++ assignment/prototypes.js
@@ -8,19 +8,28 @@ function Animal(first, last, type) {
 var giantCactus = new Animal( "Giant", "Cactus", "Dog" );
 
 // PART 1
-// create a new animal named "somethingFun" with a first name of "Something" a last name of "Fun" and a type of "Cat"
+var somethingFun = new Animal("Something", "Fun", "Cat");
 
 // PART 2
-// console log out somethingFun's first and last name
+console.log(somethingFun.firstName);
+console.log(somethingFun.lastName);
 
 // PART 3 (Hard Mode)
-// add a method to somethingFun named "meow()" that console logs "moew" when called. ex: somethingFun.meow()
+Animal.prototype.Meow = function() {
+  console.log("Meow");
+};
 
 // PART 4 (Pro Mode)
-// add another method to somethingFun named "isHappy" that receives a boolean argument "happy"
-// within this method, check if "happy" is true
-// if so, console log "purrrrrr"
-// if not, console log "crabby face"
+Animal.prototype.isHappy = function(happy) {
+  if (happy === true) {
+    console.log("purrrrrr");
+  } else {
+    console.log("crabby face");
+  }
+};
+
+somethingFun.Meow();
+somethingFun.isHappy(false);
 
 // DO NOT TOUCH CODE BELOW
 // for purpose of the test files only
